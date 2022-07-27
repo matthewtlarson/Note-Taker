6b6b173c90563e8277a589cc3a2e02ddb40dd7f2
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-07-26 19:03:26 -0700

    API route created

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,12 @@
-const path = require('path');
+const path = require('path'); //conufsed 
 const app = require('express').Router();
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html')); //dirname?
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html')); //path working?
+});
+
+module.exports = app;

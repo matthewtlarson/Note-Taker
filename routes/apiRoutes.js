e403e872cde09ad3a96d0c54a42e51f354175745
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-10 09:48:07 -0700

    office hours/first route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,14 @@
 const app = require('express').Router();
 const fs = require('fs');
-let db = require('../db/db.json');
+
+
+app.get('/api/notes'), (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", function (err, notes) {
+    if (err) {
+      throw err;
+    }
+    res.json(JSON.parse(notes));
+  })
+}
 
 module.exports = app;

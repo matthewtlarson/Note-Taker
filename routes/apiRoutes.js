983295d10a5c6d05d7412d4ce1318fe89a0511a7
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-10 09:53:44 -0700

    new post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,22 @@
 const app = require('express').Router();
 const fs = require('fs');
 
-
-app.get('/api/notes'), (req, res) => {
-  fs.readFile("./db/db.json", "utf-8", function (err, notes) {
+app.get('/api/notes', (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", function (err, notes) { 
     if (err) {
       throw err;
     }
     res.json(JSON.parse(notes));
   })
-}
+});
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile("./db/db.json"), "utf-8", function (err, notes) {
+    if (err){
+      throw err;
+    }
+    //stuck
+  }
+})
 
 module.exports = app;
